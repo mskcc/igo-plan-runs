68a63fb92bf69647934f539ed3a1354d709efd40
xshirl <xshirl525@gmail.com>
2021-05-03 19:03:46 -0400

    check capacity of lanes

--- backend/components/barcodeCollisions.js
+++ backend/components/barcodeCollisions.js
@@ -1,3 +1,4 @@
+const { Lane } = require("./Lane");
 const { Sample } = require("./Sample");
 
 
@@ -12,32 +13,60 @@ const { Sample } = require("./Sample");
 function splitBarcodes(samples, numLanes, capacity) {
     let freq = {}
     let ans = {}
+    let remaining = [];
     let minLength = samples[0].barcodeSeq.length;
     for(let sample of samples) {
         if(minLength > sample.barcodeSeq.length) {
             minLength = sample.barcodeSeq.length;
         }
     }
+    
     for(let i = 0; i < samples.length; i++) {
-        if(samples[i].barcodeSeq.substring(0, minLength) in freq) {
-            if (freq[samples[i].barcodeSeq.substring(0, minLength)] + 1 > numLanes) {
-                console.log("too many collisions");
+        let fragment = samples[i].barcodeSeq.substring(0, minLength);
+        if(fragment in freq) {
+            if (freq[fragment] + 1 > numLanes) {
+                remaining.push(samples[i]);
+                break;
             }
         }
-        if(samples[i].barcodeSeq.substring(0, minLength) in freq) {
-            freq[samples[i].barcodeSeq.substring(0, minLength)] += 1;
+        if(fragment in freq ) {
+            freq[fragment] += 1;
+           
+            
         } else {
-            freq[samples[i].barcodeSeq.substring(0, minLength)] = 1;
+            freq[fragment] = 1;
+            
         }
         console.log("freq", freq);
-        if(freq[samples[i].barcodeSeq.substring(0, minLength)] in ans) {
-            ans[freq[samples[i].barcodeSeq.substring(0, minLength)]].push(samples[i])
+        
+        
+        if(freq[fragment]in ans) {
+            
+            ans[freq[fragment]].push(samples[i])
+            
+
         } else {
-            ans[freq[samples[i].barcodeSeq.substring(0, minLength)]] = [samples[i]];
+            
+                ans[freq[fragment]] = [samples[i]];
+            
+           
         }
     }
 
-    return Object.values(ans);
+    let lanes = [];
+    for (let samples of Object.values(ans)) {
+        let totalReads = capacity;
+        for(let sample of samples) {
+            totalReads -= sample.readsRequested;
+        }
+        if(totalReads < 0) {
+            let excludedSample = samples.pop()
+            remaining.push(excludedSample);  
+        }
+        let lane = new Lane()
+    }
+    
+    return [Object.values(ans), remaining];
 }
 
 
@@ -45,8 +74,10 @@ let sample1 = new Sample(1, "", "ACTAGC", "123", "PE150", 200, "ABC", "ABC", 120
 let sample2 = new Sample(2, "", "ACTAGC-GCTACD", "123", "PE150", 200, "ABC", "ABC", 120, "nM")
 let sample3 = new Sample(3, "", "ACTAGT", "123", "PE150", 200, "ABC", "ABC", 120, "nM")
 let sample4 = new Sample(4, "", "ACTAGT-ACTTCA", "123", "PE150", 200, "ABC", "ABC", 120, "nM")
+let sample5 = new Sample(5, "", "ACTAGA", "123", "PE150", 200, "ABC", "ABC", 120, "nM")
+let sample6 = new Sample(6, "", "ACTAGC", "123", "PE150", 200, "ABC", "ABC", 120, "nM");
+console.log(splitBarcodes([sample1, sample2, sample3, sample4, sample5, sample6], 2, 400))
 
-console.log(splitBarcodes([sample1, sample2, sample3, sample4], 2, 400))
 module.exports = {
     splitBarcodes
 }
