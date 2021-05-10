f5e5a5ab82aa428f3289b244047b3462dfbe5980
xshirl <xshirl525@gmail.com>
2021-05-10 09:46:32 -0400

    cleaned up code

--- backend/components/comboSum.js
+++ backend/components/comboSum.js
@@ -2,60 +2,19 @@ const { Project } = require('./Project');
 
 /**
  * find all combinations that sum up to the target within the range (max - min read capacity)
- * @param  {Array} arr  input array of projects' total reads of samples
+ * @param  {Array} arr  input array of projects
  * @param  {Number} target  target sum
  * @param  {Number} range  range that sum needs to be within of target
- * @return {Array}  return array of combinations that add up to target within range
+ * @return {Array}  return array of indices that correspond to reads that fit run
  */
-
-// var combinationSum = function(arr, target, range) {
-//     // for(let project of arr) {
-//     //     project.getProjectReads();
-    
-//     //   }
-    //   arr.sort((a,b) => {
-    //     let A = a.totalReads;
-    //     let B = b.totalReads;
-    //     return A < B ? 1 : A > B ? -1 : 0;
-    //   })
-//     let result = [];
-//     visited = new Array(arr.length).fill(false);
-        
-//     function dfs(current, currentSum, startIndex, visited){
-//         if (target-currentSum <= range && currentSum <= target) {
-//             result.push(current)
-//         }
-//         if (currentSum > target) {
-//             return;
-//         }
-//         for (let i = startIndex; i < arr.length; i++) {
-//             if(i > 0 && arr[i].totalReads == arr[i - 1].totalReads && !visited[i - 1]) {
-//                 continue;
-//             }
-//             visited[i] = true;
-//             dfs(current.concat(arr[i]), currentSum + arr[i].totalReads, i + 1, visited);
-//             visited[i] = false;
-//         }
-//     }
-    
-//     dfs([], 0, 0, visited);
-//     return result;
-// };
 
 
 function sumArrReads(projects) { // sum up the reads in a project
-    // for(let project of projects) {
-    //     project.getProjectReads();
-    // }
+
     return projects.reduce((acc, el) => {return acc + el.totalReads}, 0);
   }
 
   let INF = 1000000000;
-  let MAX_ARR_LENGTH = 100;
-  let MAX_TARGET = 10000;
-  
-  
-  
   
   var combinationSum = function(arr, target, range) {
       let minCapacity = target - range;
@@ -68,10 +27,7 @@ function sumArrReads(projects) { // sum up the reads in a project
         sel[i] = new Array(target + 1).fill(0);
         dp[i] = new Array(target + 1).fill(-1);
       }
-     
-      // O(maxCapacity * len(arr)) = 10^4 * 100 = 10^6
-      // O(maxCapacity * len(arr))
-      // 10^7 ops / sec
+
       function rec(arr, index, remainingCapacity) {
         if (remainingCapacity < 0) {
           return INF;
@@ -86,11 +42,9 @@ function sumArrReads(projects) { // sum up the reads in a project
         let minValue = INF;
         if (a < b) {
           sel[index][remainingCapacity] = false;
-          // console.log("False", index, remainingCapacity);
           minValue = a;
         } else {
           sel[index][remainingCapacity] = true;
-          // console.log("True", index, remainingCapacity);
           minValue = b;
         }
         dp[index][remainingCapacity] = minValue;
@@ -99,59 +53,20 @@ function sumArrReads(projects) { // sum up the reads in a project
    
    
       let selectedCapacity = target - rec(arr, 0, target);
-      // console.log("selected", selectedCapacity);
       if (selectedCapacity < minCapacity || selectedCapacity > target) {
         return [];
       }
   
-      // console.log(sel);
-  
-     
       let result = [];
       let current = target;
       for (let index = 0; index < arr.length; index++) {
-        // console.log(index, current, sel[index][current], dp[index][current]);
         if (sel[index][current]) {
           result.push(index);
           current -= arr[index].totalReads;
         }
       }
-      // console.log(result);
       return result;
     }
-// var combinationSum = function(arr, target, range) {
-    
-//     if (!arr || !arr.length) { return []; }
-    
-//     // for(let project of arr) {
-//     //     project.getProjectReads();
-    
-//     //   }
-//       arr.sort((a,b) => {
-//         let A = a.totalReads;
-//         let B = b.totalReads;
-//         return A < B ? 1 : A > B ? -1 : 0;
-//       })
-
-//     // arr.sort((a,b) => a-b);
-//     const solutions = [];
-    
-//     const findCombos = function(idx, subtotal, solution) {
-//         for (let i = idx; i < arr.length; i++) {
-//             if (subtotal + arr[i].totalReads <= target && (target - (subtotal+arr[i].totalReads)) <= range) { 
-//                 solutions.push(solution.concat(arr[i])); 
-//             } else if (subtotal + arr[i].totalReads < (target-range) && i + 1 < arr.length) { 
-//                 findCombos(i + 1, subtotal + arr[i].totalReads, solution.concat(arr[i])); 
-//             }
-//             while (arr[i + 1] === arr[i]) { i++; }
-//         };
-//     };
-
-//     findCombos(0, 0, []);
-//     return solutions;
-    
-// };
-
 
 
   module.exports = {
